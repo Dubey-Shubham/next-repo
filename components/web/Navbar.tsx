89896bc2d9d4745c32213a4ce939259a8b763b5b
Shubham Dubey <shubham.dubey@acetechindia.com>
2026-03-26 17:54:54 +0530

    theme added

--- components/web/Navbar.tsx
+++ components/web/Navbar.tsx
@@ -1,11 +1,12 @@
 import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import { ThemeToggle } from "./theme-toggle";
 
 export function Navbar() {
   return (
-    <nav className="w-full border-b bg-white">
+    <nav className="w-full border-b">
       <div className="px-4 h-16 flex items-center justify-between">
-        
+
         {/* Left - Logo */}
         <div className="text-xl font-bold text-blue-500">
           ReactJS
@@ -13,24 +14,17 @@ export function Navbar() {
 
         {/* Center - Links */}
         <div className="hidden md:flex gap-6 text-sm font-medium text-black">
-          <a href="/" className="hover:text-blue-500 transition">
-            Home
-          </a>
-          <a href="/about" className="hover:text-blue-500 transition">
-            About
-          </a>
-          <a href="/services" className="hover:text-blue-500 transition">
-            Services
-          </a>
-          <a href="/contact" className="hover:text-blue-500 transition">
-            Contact
-          </a>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/auth/sign-up">Home</Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/about">About</Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/services">Services</Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/contact">Contact</Link>
         </div>
 
         {/* Right - Buttons */}
         <div className="flex gap-3">
           <Link className={buttonVariants()} href="/auth/sign-up">Sign Up</Link>
-          <Link className={buttonVariants()} href="/auth/login">Login</Link>
+          <Link className={buttonVariants({ variant: "outline" })} href="/auth/login">Login</Link>
+          <ThemeToggle/>
         </div>
 
       </div>
